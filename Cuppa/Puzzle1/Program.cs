54a4aa9f24e6d6e7cfe4a11b54305ea3a1db36f6
Mark Leighton <markconnorleighton@gmail.com>
2023-11-17 12:51:41 +0000

    Outputs the correct value

---
+++ Cuppa/Puzzle1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System;
+
+
+//var path = Path.Combine(Directory.GetCurrentDirectory(), "inputtext.txt");
+
+string[] lines = File.ReadAllLines("C:/Users/markc/source/repos/CuppaFirst/Cuppa/Puzzle1/inputtext.txt");
+
+int max = 0;
+int cur = 0;
+
+foreach (string line in lines)
+{
+    if(line != "")
+    {
+        int x = int.Parse(line);
+        cur += x;
+    }
+
+    if (line == "" && cur > max)
+    {
+        max = cur;
+        cur = 0;
+    }
+}
+
+Console.WriteLine(max);
